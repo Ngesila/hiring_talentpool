5e2089e5844f980e6203e25e37b28bd78b6cf4ab
Ngesila <>
2023-07-10 18:09:18 +0000

    Add validation to check for exactly 4 values in provided data

--- run.py
+++ run.py
@@ -11,29 +11,78 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hiring_talentpool')
 
-#applicants = SHEET.worksheet('applicants')
+# applicants = SHEET.worksheet('applicants')
 
-#data = applicants.get_all_values()
+# data = applicants.get_all_values()
 
-#print(data)
+# print(data)
+
 
 def get_applicants_data():
     """
     Get applicants information from the user
     """
     print("Please enter applicants information from user")
-    print("Name should be two names")
-    print("Example: James Peter")
+    print("name should be two names")
+    print("example: James Peter")
     print("years of experience should be interger")
-    print("Example: 5")
-    print("Development languages should be interger")
-    print("Example: 4")
-    print("Foreign languages should be interger")
-    print("Example: 3")
+    print("example: 5")
+    print("development languages should be interger")
+    print("example: 4")
+    print("foreign languages should be interger")
+    print("Example: 3\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
-
-get_applicants_data()
 
 
+    applicants_data = data_str.split(",")
+    validate_data(applicants_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into intergers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly values.
+    """
+    try:
+        if len(values) != 4:
+            raise ValueError(
+            f"Exactly 4 values required, you provided {len(values)}"
+        )
+    except ValueError as e:
+        print(f"Invalid data:{e}, please try again.\n")
+
+get_applicants_data()        
+                
+                
+    
+    
+
+    
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+   
+    
+    
+    
+
+    
+
+    
+   
