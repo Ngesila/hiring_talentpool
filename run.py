b0e047881be3487bb1fe4593d85a9c01690b910c
Ngesila <>
2023-07-13 09:59:47 +0000

    Adjust validation to check that all values provided are integers

--- run.py
+++ run.py
@@ -11,33 +11,27 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hiring_talentpool')
 
-# applicants = SHEET.worksheet('applicants')
-
-# data = applicants.get_all_values()
-
-# print(data)
-
 
 def get_applicants_data():
     """
     Get applicants information from the user
     """
-    print("Please enter applicants information from user")
-    print("name should be two names")
-    print("example: James Peter")
-    print("years of experience should be interger")
-    print("example: 5")
-    print("development languages should be interger")
-    print("example: 4")
-    print("foreign languages should be interger")
-    print("Example: 3\n")
+    while True:
+        try:
+            name = input("Please enter your name: \n")
+            if len(name) < 2:
+                raise ValueError("Invalid input. Please enter a name with more than 2 characters.")
+        except ValueError as e:
+            print(f"Invalid input: {e}, Please enter a name with more than 2 characters.")
+            continue
+        else:
+            break
 
-    data_str = input("Enter your data here: ")
+    data_str = input("Please enter the following numbers: \n years of experience, development languages, foreign languages: \n ")
 
 
     applicants_data = data_str.split(",")
     validate_data(applicants_data)
-
 
 def validate_data(values):
     """
@@ -45,25 +39,26 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly values.
     """
+    
     try:
-        if len(values) != 4:
-            raise ValueError(
-            f"Exactly 4 values required, you provided {len(values)}"
-        )
+        [int(value) for value in values]
+        if len(values) != 3:
+            raise ValueError("Exactly 3 values required, you provided {len(values)}")
     except ValueError as e:
         print(f"Invalid data:{e}, please try again.\n")
 
-get_applicants_data()        
+get_applicants_data()     
+
+    
+
+   
+   
+    
+    
+    
+
+   
                 
-                
-    
-    
-
-    
-
-    
-
-    
 
 
 
